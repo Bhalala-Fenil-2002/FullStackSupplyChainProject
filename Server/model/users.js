328fa0e2a2914935354bb0d12940a78163d99c85
Bhalala-Fenil-2002 <bhalala.fenil2002@gmail.com>
2023-07-15 18:34:36 +0530

    feat(singin & signup): mongodb database complete.

---
+++ Server/model/users.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const UserSchema = mongoose.Schema({
+    fname: { type: String, required: true },
+    lname: { type: String, required: true },
+    gender: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
+})
+
+// UserSchema.plugin(uniqueValidator);
+
+const USERSCHEMA = mongoose.model('users', UserSchema);
+
+module.exports = USERSCHEMA;
--- UI/client/src/App.jsx
+++ UI/client/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { EthProvider } from "./contexts/EthContext";
 import Intro from "./components/Intro/";
